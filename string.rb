31f2b0d4f71a53a01abe4b92ffc60c4c8690209e
rodrigoSilva23 <rodriguinho2456@gmail.com>
2021-07-16 16:45:03 -0300

    roby

---
+++ string.rb
@@ -0,0 +1,14 @@
+#as aspa dublas é usando principal menta para fazer uma interpolação 
+nome = "rodrigo"
+menssagem = "bem vindo #{nome}"
+
+
+puts menssagem
+
+menssagem2 = <<-MENSSAGEM
+vamos testa isso ae.
+
+ae deu certo kkk!!!
+MENSSAGEM
+
+puts menssagem2
